247b13c63a92777e6debac56e6fa38683a2ffe5c
Obiana Ifeanyi <Ifeanyiobiana@gmail.com>
2023-06-19 20:15:39 +0100

    Update push_docker.sh

--- push_docker.sh
+++ push_docker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+REGION="us-east-1"
+
 aws ecr create-repository 
   --repository-name gris-repo \
-  --region us-east-1 \
+  --region $REGION \
   --output text
   
 
@@ -12,6 +14,6 @@ docker run --name gris-container -t -i -p 80:80 helloo-world
 
 docker tag helloo-world:latest 914209118176.dkr.ecr.us-east-1.amazonaws.com/gris-repo
 
-docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 914209118176.dkr.ecr.us-east-1.amazonaws.com/gris-repo
+docker login -u AWS -p $(aws ecr get-login-password --region $REGION) 914209118176.dkr.ecr.us-east-1.amazonaws.com/gris-repo
 
 docker push 914209118176.dkr.ecr.us-east-1.amazonaws.com/gris-repo
